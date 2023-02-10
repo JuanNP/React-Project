6f260c7c2d3f147a2e4abbe9056b14f5cd3720fe
JuanNP <20198547@itla.edu.do>
2023-02-09 23:30:21 -0400

    SideBar Component y Navbar Component. Se agrego el componente SideBar y Navbar. Se modifico el componente App.js para que se muestre el componente SideBar y Navbar. Se modifico el componente index.js para que se muestre el componente App.js. Se modifico el componente index.css para que se muestre el componente SideBar y Navbar. Se modifico el componente index.html para que se muestre el componente SideBar y Navbar. Se modifico el componente package.json para que se muestre el componente SideBar y Navbar. Se modifico el componente package-lock.json para que se muestre el componente SideBar y Navbar.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { Box, Container, Stat, Text, StatHelpText, Heading, Button } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container centerContent>
+      <Box maxW='sm' borderRadius='10' borderStyle='solid' borderWidth='1px' borderColor='lightgrey' overflow='hidden' bg='white' pl='20' w='40vw' mt='50px'>
+        <Box p='6'>
+          <Box display='flex' alignItems='baseline'>
+            <Stat>
+              <Heading
+                color='gray.500'
+                fontWeight='medium'
+                fontSize='1.45rem'
+                letterSpacing='wide'
+                mt='0'
+              >
+                Saldo actual
+              </Heading>
+              <Text
+                color='gray.900'
+                fontWeight='bold'
+                fontSize='3rem'
+                letterSpacing='wide'
+                fontFamily='roboto'
+                m='0'
+              >
+              $ 1,000,000
+              </Text>
+              <Text 
+                color='gray.500'
+                fontWeight='medium'
+                fontSize='1.1rem'
+                letterSpacing='wide'
+              >
+                Disponible
+              </Text>
+              <Button 
+                width='6vw' 
+                height='3vh' 
+                borderRadius='20px'
+                fontSize='15px'
+                fontWeight='bold'
+                color='white'
+                bg='#00376f'
+                p='20px'
+                mt='20px'
+
+                _hover={{
+                  transform: 'scale(1.05)',
+                  transition: '0.3s',
+                  bg: '#034F9B',
+                  cursor: 'pointer'
+                }}
+
+              >
+                Transferir
+              </Button>
+            </Stat>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default App;
