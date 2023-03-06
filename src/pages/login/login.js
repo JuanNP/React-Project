f7849b925961728055266f13e97bf051f59c8b2d
JuanNP <20198547@itla.edu.do>
2023-03-05 20:14:26 -0400

    Fondo de pantalla de login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -4,10 +4,12 @@ import{makeStyles} from '@material-ui/core/styles'
 import fondo from '../../assets/images/fondo.jpg'
 import { LockOutlined as LockOutlinedIcon} from '@mui/icons-material'
 import {Link} from 'react-router-dom'
+import { transform } from 'framer-motion'
+import { transition } from '@chakra-ui/react'
 
 const useStyles = makeStyles(theme=>({
   root:{
-    backgroundImage: `url(${fondo})`,
+    background: 'linear-gradient(90deg, rgba(0,55,111,1) 25%, rgba(0,212,255,1) 100%)',
     backgroundRepeat: 'no-repeat' ,
     backgroundSize: 'cover',
     backgroundPosition: 'center' ,
@@ -30,14 +32,22 @@ const useStyles = makeStyles(theme=>({
   },
     avatar:{
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.primary.main
+    backgroundColor: '#00376f'
   },
   form:{
       width: '100%',
       marginTop: theme.spacing(1)
   },
   button:{
-    margin: theme.spacing(3,0,2)
+    margin: theme.spacing(3,0,2),
+    backgroundColor: '#00376f',
+    color: '#fff',
+    '&:hover':{
+      backgroundColor: '#00376f',
+      color: '#fff',
+      transform: 'scale(1.05)',
+      transition: 'all 0.3s ease-in-out'
+    }
   }
 }))
 
@@ -56,7 +66,7 @@ const Login = () => {
             <TextField fullWidth autoFocus color='primary' margin='normal' variant='outlined' label='introduzca su usuario' name='nombre'/>
             <TextField fullWidth type='password' color='primary' margin='normal' variant='outlined' label='introduzca su contraseña' name='contra'/>
             <Link to='/dashboard'>
-              <Button fullWidth variant='contained' color='primary' className={classes.button}>
+              <Button fullWidth variant='contained' className={classes.button}>
                   Sign In
               </Button>
             </Link>
