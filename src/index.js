05b5b4a53870f0207566e613e2d8488efbb1add6
JuanNP <20198547@itla.edu.do>
2023-03-16 21:58:23 -0400

    ................

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-<<<<<<< HEAD
 import { RouterProvider } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ROUTER } from './router/router';
@@ -13,22 +12,9 @@ root.render(
     <ChakraProvider>
       <RouterProvider router={ROUTER} />
     </ChakraProvider>
-=======
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
->>>>>>> 058f32e (Initialize project using Create React App)
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-<<<<<<< HEAD
-=======
-reportWebVitals();
->>>>>>> 058f32e (Initialize project using Create React App)
