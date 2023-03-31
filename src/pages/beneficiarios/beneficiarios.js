cf34aa568440a714b50b45fe110e4aa1ef9aafc7
reylishernandez29 <reylisfrancisco@outlook.es>
2023-03-31 16:43:46 -0400

    benefisiario

--- src/pages/beneficiarios/beneficiarios.js
+++ src/pages/beneficiarios/beneficiarios.js
@@ -1,11 +1,46 @@
 import { Container } from "@chakra-ui/react";
-
+import "./estilos.css";
 
 export const Beneficiarios = () => {
   return (
     <>
       <Container centerContent>
-        <h1>Beneficiarios</h1>
+        <p class ="titulo">
+          Beneficarios
+        </p>
+
+        <div class="beneficiario">
+          <div class="beneficiario-form">
+            <h3>Crear beneficiario</h3>
+            <form action="">
+              <div>
+                <p>
+                  <label for="cuenta">Cuenta o Contrato</label>
+                  <input type="text" name="cuenta" id="cuenta"></input>
+                </p>
+                <p>
+                  <label for="cedula"> Cedula</label>
+                  <input type="text" name="cedula" id="cedula"></input>
+                </p>
+              </div>
+              <div>
+                <p>
+                  <label for="alias">Alias</label>
+                  <input type="text" name="alias" id="alias"></input>
+                </p>
+                <p>
+                  <label for = "correo">Correo electronico</label>
+                  <input type="text" name="correo" id="correo"></input>
+                </p>
+                
+              </div>
+        
+
+                <button type="submit">Guardar</button>
+             
+            </form>
+          </div>
+        </div>
       </Container>
     </>
   );
